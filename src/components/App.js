e65271a3712973f431bf4d73ea26dcce703e5203
David Roussov <david.roussov@hotmail.com>
2017-08-06 12:55:22 +1000

    added basic calendar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import calendarStyle from '../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    BigCalendar.momentLocalizer(moment);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello, World!*</h1>
+      <div style={{padding: '40px'}}>
+        <BigCalendar
+          events={[]}
+          startAccessor='startDate'
+          endAccessor='endDate'
+          style={{height: '800px'}}
+        />
       </div>
     )
   }
