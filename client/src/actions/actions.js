444dcbd49807a316d055f772026be2451fa16044
David Roussov <david.roussov@hotmail.com>
2017-08-22 18:48:12 +1000

    client now gets event data from server

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,7 +1,5 @@
 export function getEventData() {
-  console.log('action called');
   return dispatch => {
-
     fetch('http://localhost:8080/customer/calendar/get', {
       method: 'GET',
       mode: 'cors',
@@ -10,108 +8,11 @@ export function getEventData() {
       }
     })
     .then(response => response.json())
-    .then(response => {
-      console.log(JSON.stringify(response, null, 2));
-    });
-
-
-    // replace following with a fetch (GET)
-    dispatch({
-      type: 'GET_CALENDAR_EVENTS',
-      eventData
+    .then(eventData => {
+      dispatch({
+        type: 'GET_CALENDAR_EVENTS',
+        eventData
+      });
     });
   }
 }
-
-const eventData = [
-  {
-    "id": "",
-    "title": "a title",
-    "start": "2017-12-26",
-    "end": "2017-12-26",
-    "category": "Public holiday",
-    "label": "some holiday",
-    "readonly": true
-  },
-  {
-    "id": "",
-    "title": "two title",
-    "start": "2017-08-30",
-    "end": "2017-09-04",
-    "category": "RDO",
-    "label": "RDO",
-    "readonly": false
-  },
-  {
-    "id": "",
-    "title": "three title",
-    "start": "2017-09-05",
-    "end": "2017-09-05",
-    "category": "public holiday",
-    "label": "erhgegerh",
-    "readonly": true
-  },
-  {
-    "id": "",
-    "title": "four title",
-    "start": "2017-09-13",
-    "end": "2017-09-14",
-    "category": "some category",
-    "label": "some event",
-    "readonly": false
-  },
-  {
-    "id": "",
-    "title": "five title",
-    "start": "2017-10-03",
-    "end": "2017-10-03",
-    "category": "public holiday",
-    "label": "hello",
-    "readonly": true
-  },
-  {
-    "id": "",
-    "title": "six title",
-    "start": "2017-11-03",
-    "end": "2017-11-11",
-    "category": "some category",
-    "label": "long event",
-    "readonly": false
-  },
-  {
-    "id": "",
-    "title": "seven title",
-    "start": "2017-12-03",
-    "end": "2017-12-05",
-    "category": "efnefee",
-    "label": "gsergregre",
-    "readonly": false
-  },
-  {
-    "id": "",
-    "title": "eight title",
-    "start": "2017-12-04",
-    "end": "2017-12-06",
-    "category": "public holiday",
-    "label": "",
-    "readonly": false
-  },
-  {
-    "id": "",
-    "title": "nine title",
-    "start": "2017-12-16",
-    "end": "2017-12-16",
-    "category": "public holiday",
-    "label": "",
-    "readonly": true
-  },
-  {
-    "id": "",
-    "title": "ten title",
-    "start": "2017-12-17",
-    "end": "2017-12-17",
-    "category": "esrgsregr",
-    "label": "sergreg",
-    "readonly": false
-  }
-];
